206e458d914650a8c5a8b7f2ed5c695da4588db2
dravit <dravit.info@gmail.com>
2018-03-15 16:12:42 +0530

    Add files via upload

--- src/main/java/com/rbs/global/DatabaseConfig.java
+++ src/main/java/com/rbs/global/DatabaseConfig.java
@@ -1,1 +1,57 @@
+package com.rbs.global;
 
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+import java.util.Collections;
+
+/**
+ * Created by dgup27 on 3/15/2017.
+ */
+@Configuration
+@EnableMongoRepositories("com.rbs.repositories")
+@PropertySource(value = { "classpath:application.properties" })
+public class DatabaseConfig  extends AbstractMongoConfiguration {
+
+    @Value("${spring.data.mongodb.host}")
+    private String host;
+    @Value("${spring.data.mongodb.port}")
+    private Integer port;
+    @Value("${spring.data.mongodb.username}")
+    private String username;
+    @Value("${spring.data.mongodb.database}")
+    private String database;
+    @Value("${spring.data.mongodb.password}")
+    private String password;
+
+    @Bean
+    public ValidatingMongoEventListener validatingMongoEventListener() {
+        return new ValidatingMongoEventListener(validator());
+    }
+    @Bean
+    public LocalValidatorFactoryBean validator() {
+        return new LocalValidatorFactoryBean();
+    }
+
+    @Override
+    public String getDatabaseName() {
+        return database;
+    }
+    @Override
+    @Bean
+    public Mongo mongo() throws Exception {
+        return new MongoClient(Collections.singletonList(new ServerAddress(host, port)),
+                Collections.singletonList(MongoCredential.createCredential(username,database, password.toCharArray())));
+    }
+
+}
