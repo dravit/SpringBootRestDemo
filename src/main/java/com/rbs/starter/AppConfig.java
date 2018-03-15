a3159469d5eef7700fbaeaf05e0660099e515067
dravit <dravit.info@gmail.com>
2018-03-15 16:18:52 +0530

    Add files via upload

--- src/main/java/com/rbs/starter/AppConfig.java
+++ src/main/java/com/rbs/starter/AppConfig.java
@@ -1,1 +1,22 @@
+package com.rbs.starter;
 
+import org.jsondoc.spring.boot.starter.EnableJSONDoc;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by dgup27 on 3/15/2017.
+ */
+@Configuration
+@EnableJSONDoc
+@EnableAutoConfiguration
+@ComponentScan(basePackages = "com.rbs")
+public class AppConfig {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppConfig.class, args);
+    }
+
+}
