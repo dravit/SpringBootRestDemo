b731c72e595aee80c240b82afe7266d1b979aa67
dravit <dravit.info@gmail.com>
2018-03-15 16:17:53 +0530

    Add files via upload

--- src/main/java/com/rbs/services/CustomerService.java
+++ src/main/java/com/rbs/services/CustomerService.java
@@ -1,1 +1,13 @@
+package com.rbs.services;
 
+import com.rbs.objects.Customer;
+
+/**
+ * Created by dgup27 on 3/15/2017.
+ */
+public interface CustomerService {
+
+    Customer findById(String id);
+
+    Customer save(Customer customer);
+}
